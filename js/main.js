9043ae758ce40257f5e3d38f36423bfe3f0c354a
GeoEko <georg2007e@gmail.com>
2024-06-21 20:59:58 +0300

    add  team&Blog

--- js/main.js
+++ js/main.js
@@ -58,3 +58,12 @@ $(document).ready(function () {
       }
   })
 });
+//Слайдер
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: false,
+  dots: true,
+  dotsClass: 'dots-style'
+});
